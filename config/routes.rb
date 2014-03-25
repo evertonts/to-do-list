93dcb7c9fd1986d5f2eec573d493ca1a6980f6ba
everton <everton2x4@gmail.com>
2014-03-24 23:20:25 -0300

    Criando as tasks

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ ToDoList::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'tasks#index'
+  
+  resources :tasks do
+    get 'index'
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
